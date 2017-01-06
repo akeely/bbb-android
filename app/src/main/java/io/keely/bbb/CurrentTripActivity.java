fa5e893a914fe07e2d4eab80b9329ee5b685471b
Andrew Keely <akeely@rapid7.com>
2017-01-06 15:02:22 -0500

    Progress on creating a trip

---
+++ app/src/main/java/io/keely/bbb/CurrentTripActivity.java
@@ -0,0 +1,13 @@
+package io.keely.bbb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CurrentTripActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_current_trip);
+    }
+}
--- app/src/main/java/io/keely/bbb/NewTripActivity.kt
+++ app/src/main/java/io/keely/bbb/NewTripActivity.kt
@@ -1,9 +1,14 @@
 package io.keely.bbb
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_new_trip.*
+import java.util.*
 
 class NewTripActivity : AppCompatActivity() {
 
@@ -12,7 +17,30 @@ class NewTripActivity : AppCompatActivity() {
         setContentView(R.layout.activity_new_trip)
 
         val names = resources.getStringArray(R.array.boys_members)
-        val adapter = ArrayAdapter(this, R.layout.namerow, names)
+        val adapter = ArrayAdapter(this, R.layout.select_name_row, names)
         boysSelectionListView.adapter = adapter
+        boysSelectionListView.onItemClickListener = AdapterView.OnItemClickListener { adapterView, view, i, l ->
+            toggleListSelection(view, i)
+        }
+    }
+
+    fun toggleListSelection(view: View, position: Int) {
+
+        val isSelected = view.isSelected
+        view.isSelected = !isSelected
+    }
+
+    fun startNewTrip(view: View) {
+
+        val intent = Intent(this, CurrentTripActivity::class.java)
+        val nameList = boysSelectionListView.checkedItemIds
+                .map {id -> id.toInt()}
+                .map {id -> findViewById(id) as TextView }
+                .map {textView -> textView.text }
+                as ArrayList
+
+        intent.putCharSequenceArrayListExtra("trip.names", nameList)
+
+        startActivity(intent)
     }
 }
